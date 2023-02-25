f0ecd62695e261f72c6a107c0d06502746bbeb34
Orchi1904 <alexej1@web.de>
2023-02-25 12:39:37 +0100

    Added some HTML-Landmarks

--- src/App.js
+++ src/App.js
@@ -13,17 +13,21 @@ function App() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@700&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Outfit&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Outfit&display=swap" rel="stylesheet" />
       </Helmet >
-      <div className="appContainer">
-        <img src={QrCode} alt="QR-Code to Frontend Mentor" />
-        <h2>Improve your front-end skills by building projects</h2>
-        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
-      </div>
-      <div className="attribution">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-        Coded by <a href="https://github.com/Orchi1904" target="_blank">Alexej Kunz (Orchi1904)</a>.
-      </div>
+      <main>
+        <div className="appContainer">
+          <img src={QrCode} alt="QR-Code to Frontend Mentor" />
+          <h2>Improve your front-end skills by building projects</h2>
+          <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+      </main>
+      <footer>
+        <div className="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+          Coded by <a href="https://github.com/Orchi1904" target="_blank">Alexej Kunz (Orchi1904)</a>.
+        </div>
+      </footer>
     </div >
   );
 }
